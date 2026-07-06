299b6f5e352e71acf2bd258bce386f9db7cec9d7
Ahmedzagzoug1 <zagzougprograming@gmail.com>
2026-07-06 03:50:06 +0300

    ADD Flutter secure storage to save tokens

---
+++ lib/core/storage/secure_storage_service.dart
@@ -0,0 +1,52 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:injectable/injectable.dart';
+
+@lazySingleton
+class SecureStorage {
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+
+  static const String accessTokenKey = 'access_token';
+  static const String refreshTokenKey = 'refresh_token';
+
+  Future<void> saveAccessToken(String token) async {
+    await _storage.write(
+      key: accessTokenKey,
+      value: token,
+    );
+  }
+
+  Future<String?> getAccessToken() async {
+    return await _storage.read(
+      key: accessTokenKey,
+    );
+  }
+
+  Future<void> saveRefreshToken(String token) async {
+    await _storage.write(
+      key: refreshTokenKey,
+      value: token,
+    );
+  }
+
+  Future<String?> getRefreshToken() async {
+    return await _storage.read(
+      key: refreshTokenKey,
+    );
+  }
+
+  Future<void> deleteAccessToken() async {
+    await _storage.delete(
+      key: accessTokenKey,
+    );
+  }
+
+  Future<void> deleteRefreshToken() async {
+    await _storage.delete(
+      key: refreshTokenKey,
+    );
+  }
+
+  Future<void> clear() async {
+    await _storage.deleteAll();
+  }
+}
